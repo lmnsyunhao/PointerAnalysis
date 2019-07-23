4332a6c6999f3937e43cc3f175a482ed2ebf04ba
yunhao <851549562@qq.com>
2019-07-23 23:12:55 +0800

    change SceneTransformer

--- src/core/AnalyzerEntrance.java
+++ src/core/AnalyzerEntrance.java
@@ -6,6 +6,23 @@ import java.util.*;
 import soot.*;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 
+class WholeProgramTransformer extends SceneTransformer {
+	@Override
+	protected void internalTransform(String arg0, Map<String, String> arg1) {
+		
+		SootMethod sm = Scene.v().getMainMethod();
+		Stack<String> stk = new Stack<String>();
+		stk.push(sm.toString());
+		Analysis pa = new Analysis(new ExceptionalUnitGraph(sm.retrieveActiveBody()), new HashMap<String, Set<String>>(), sm.toString(), stk);
+		stk.pop();
+		
+		String res = pa.getResult();
+		System.out.print(res);
+		AnswerPrinter.printAnswer(res);
+		
+	}
+}
+
 public class AnalyzerEntrance {
 	public static void main(String[] args) {
 		String classpath = args[0] + File.pathSeparator
@@ -13,23 +30,7 @@ public class AnalyzerEntrance {
 				+ args[0] + File.separator + "jce.jar";
 		System.out.println(classpath);
 		
-		PackManager.v().getPack("wjtp").add(
-			new Transform("wjtp.mypta", new SceneTransformer() {
-				protected void internalTransform(String arg0, Map<String, String> arg1) {
-					
-					SootMethod sm = Scene.v().getMainMethod();
-					Stack<String> stk = new Stack<String>();
-					stk.push(sm.toString());
-					Analysis pa = new Analysis(new ExceptionalUnitGraph(sm.retrieveActiveBody()), new HashMap<String, Set<String>>(), sm.toString(), stk);
-					stk.pop();
-					
-					String res = pa.getResult();
-					System.out.print(res);
-					AnswerPrinter.printAnswer(res);
-					
-				}
-			})
-		);
+		PackManager.v().getPack("wjtp").add(new Transform("wjtp.mypta", new WholeProgramTransformer()));
 		soot.Main.main(new String[] {
 			"-w",
 //			"-p", "cg.spark", "enabled:true",
